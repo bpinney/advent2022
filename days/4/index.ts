d6a114a0aeab5ed89ec8f3784202bef7f563d418
Brian Pinney <bpinney@laborsync.com>
2022-12-05 07:23:12 -0500

    Simplify Day 4

--- days/4/index.ts
+++ days/4/index.ts
@@ -1,38 +1,29 @@
-const isWithin = (firstPair: number[], secondPair: number[]) => {
-  return (
-    (firstPair[0] >= secondPair[0] && firstPair[1] <= secondPair[1]) ||
-    (firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[1])
-  );
-};
+const isWithin = (firstPair: number[], secondPair: number[]) =>
+  (firstPair[0] >= secondPair[0] && firstPair[1] <= secondPair[1]) ||
+  (firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[1]);
 
-const overlaps = (firstPair: number[], secondPair: number[]) => {
-  return (
-    (firstPair[0] >= secondPair[0] && firstPair[0] <= secondPair[1]) ||
-    (firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[0])
-  );
-};
+const overlaps = (firstPair: number[], secondPair: number[]) =>
+  (firstPair[0] >= secondPair[0] && firstPair[0] <= secondPair[1]) ||
+  (firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[0]);
+
+const findSolution = (
+  pairs: string[][],
+  callback: (firstPair: number[], secondPair: number[]) => boolean
+) =>
+  pairs
+    .map((pair) => {
+      const [first, second] = pair
+        .map((str) => str.split("-"))
+        .map((str) => str.map((num) => +num));
+      return callback(first, second);
+    })
+    .filter((x) => x === true).length;
 
 const solve = (input: string) => {
   const inputArray = input.split("\n");
   const pairs = inputArray.map((pair) => pair.split(","));
-
-  const solution1 = pairs
-    .map((pair) => {
-      const [first, second] = pair
-        .map((str) => str.split("-"))
-        .map((str) => str.map((num) => +num));
-      return isWithin(first, second);
-    })
-    .filter((x) => x === true).length;
-
-  const solution2 = pairs
-    .map((pair) => {
-      const [first, second] = pair
-        .map((str) => str.split("-"))
-        .map((str) => str.map((num) => +num));
-      return overlaps(first, second);
-    })
-    .filter((x) => x === true).length;
+  const solution1 = findSolution(pairs, isWithin);
+  const solution2 = findSolution(pairs, overlaps);
 
   return [solution1, solution2];
 };
